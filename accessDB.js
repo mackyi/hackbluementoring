3d2a3fe8723afb59a50e0273c39a1c61e6ab7d8e
Bryan Gaston <bryangaston122@gmail.com>
2013-04-07 10:14:40 -0400

    findLesson

--- accessDB.js
+++ accessDB.js
@@ -166,6 +166,17 @@ module.exports = {
 			});	
 	},
 	
+	findLesson: function(lessonId, callback){
+	Lesson.findById(lessonId, null, null, function(err, lesson){
+		if (!err){
+			callback(null, lesson);
+		}
+		else{
+			callback(err);
+		}
+	});
+	}
+	
 	findAssignments: function(lessonId, callback){
     Assignment.find({lessonId: lessonId}, function(err, assignments){
       callback(null, assignments);
